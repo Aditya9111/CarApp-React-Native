de685d12477b8cb7a6356babc1dd2dd8ac950d9d
Aditya Arya <52771727+Aditya9111@users.noreply.github.com>
2021-04-21 01:04:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA7R1dMiE2Izek-nmxpNTjGZVUN7abpwEg",
-  authDomain: "sign-5cefb.firebaseapp.com",
-  databaseURL: "https://sign-5cefb.firebaseio.com",
-  projectId: "sign-5cefb",
-  storageBucket: "sign-5cefb.appspot.com",
-  messagingSenderId: "608986662413",
-  appId: "1:608986662413:web:fde1cf878974e8c2f77744",
-};
+
